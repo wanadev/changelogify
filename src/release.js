ff1be10203c70cfb98695d52f4095b351cd1b7e5
Jean-Baptiste Ghoul <jeanbaptiste.ghoul@gmail.com>
2025-02-12 17:22:27 +0100

    fix duplicated changelog if no issue number

--- src/release.js
+++ src/release.js
@@ -68,9 +68,10 @@ async function release({ releaseVersion, date, silent }, options) {
                 data[type].forEach(({ message, issueNumber }) => {
                     if (issueNumber === "" || !config.gitIssueTemplate) {
                         text = `${text}- ${message}\n`;
+                    } else {
+                        const link = config.gitIssueTemplate.replace(/NUMBER/g, issueNumber);
+                        text = `${text}- ${message} - ${link}\n`;
                     }
-                    const link = config.gitIssueTemplate.replace(/NUMBER/g, issueNumber);
-                    text = `${text}- ${message} - ${link}\n`;
                 });
             }
             return text;
